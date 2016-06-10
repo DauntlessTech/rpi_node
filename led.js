d748ef8c8626f4ba1b16cab3520d5c9b1dd50dba
Zachary Foutz <zacharyfoutz@gmail.com>
2016-06-10 11:51:01 -0700

    fix: reverted back to original code

--- led.js
+++ led.js
@@ -1,54 +1,17 @@
-<<<<<<< HEAD
-// var wpi = require('wiring-pi'),
-// morse = require('morse-node').create(),
-// msg = morse.encode('Hello World');
-// console.log(msg);
-//
-// wpi.setup('gpio');
-//
-// var pin = 17;
-//
-// wpi.pinMode(pin, wpi.OUTPUT);
-//
-// var value = 1;
-//
-// setInterval(function() {
-//   wpi.digitalWrite(pin, value);
-//   value = +!value;
-// }, 500);
-=======
 var wpi = require('wiring-pi'),
 morse = require('morse-node').create(),
 msg = morse.encode('Hello World');
-console.log('msg');
-<<<<<<< HEAD
->>>>>>> parent of 80f09ac... fix: remove quotes around msg in the morse code log
-=======
->>>>>>> parent of 80f09ac... fix: remove quotes around msg in the morse code log
+console.log(msg);
 
-var Blynk = require('blynk-library');
-var AUTH = "249414e434de41048be27bcbd0d44c22";
-var blynk = new Blynk.Blynk(AUTH, options= {addr:"192.168.1.118"});
+wpi.setup('gpio');
 
-var GPIO = require('onoff').Gpio,
-    led = new GPIO(17, 'out'),
-    button = new GPIO(18, 'in', 'both');
+var pin = 17;
 
-// define the callback function
-function light(err, state) {
+wpi.pinMode(pin, wpi.OUTPUT);
 
-  // check the state of the button
-  // 1 == pressed, 0 == not pressed
-  if(state == 1) {
-    // turn LED on
-    led.writeSync(1);
-  } else {
-    // turn LED off
-    led.writeSync(0);
-  }
+var value = 1;
 
-}
-
-// pass the callback function to the
-// as the first argument to watch()
-button.watch(light);
+setInterval(function() {
+  wpi.digitalWrite(pin, value);
+  value = +!value;
+}, 500);
